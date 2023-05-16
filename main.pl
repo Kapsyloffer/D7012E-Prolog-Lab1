686ec1a30001dac4b24a2298f93d695e343f1281
Kapsyloffer <CrilleMega@gmail.com>
2023-05-16 13:45:43 +0200

    Monkey code

--- main.pl
+++ main.pl
@@ -16,3 +16,50 @@
 
 hello_world :-
     write("Hello world!").
+
+%pick up the s_key in room 1
+
+%go to room 2
+
+%pick up the b_key
+
+%go to room 1
+
+%drop the s_key
+
+%go to room 3
+
+%pickup the package
+
+%go to room 1
+
+%drop the package
+
+%win
+
+
+% recursive steps
+
+move( state( middle, onbox, middle, hasnot),
+      grasp,
+      state( middle, onbox, middle, has) ).    
+
+move( state( P, onfloor, P, H),
+      climb,
+      state( P, onbox, P, H) ).   
+
+move( state( P1, onfloor, P1, H),
+      push( P1, P2),
+      state( P2, onfloor, P2, H) ).
+
+move( state( P1, onfloor, B, H),
+      walk( P1, P2),
+      state( P2, onfloor, B, H) ).                   
+
+canget( state( _, _, _, has), [done| []]).       
+
+canget( State1, [Move| Trace2])  :-
+    move( State1, Move, State2),
+    canget( State2, Trace2). 
+
+% canget(state(atdoor,onfloor,atwindow,hasnot), R).
