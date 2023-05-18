7110cad023ab1bc935bb5b4e0b800cc80e0f52e7
Kapsyloffer <CrilleMega@gmail.com>
2023-05-18 15:03:45 +0200

    I guess I am doing this right, but I get stack overflow :(

--- main.pl
+++ main.pl
@@ -1,7 +1,7 @@
 %rooms
 room(r1).
 room(r2).
-room(r2).
+room(r3).
 
 %items
 item(box).
@@ -14,15 +14,15 @@ connected(r1, r3).
 
 %the robot
 robot(B) :-
-    item(I1), item(I2). % 2 inventory slots
+    item(I1), item(I2), % 2 inventory slots
     B = hold(I1, I2).
 
 room_item(I, R) :-
     item(I), room(R).
 
 %key requirement for the doors
-requires_key(2, steelkey).
-requires_key(3, brasskey).
+requires_key(r2, steelKey).
+requires_key(r3, brassKey).
 
 %If an item is in the room
 in_room(Item, Room) :- item(Item), room(Room), \+ picked_up(Item).
@@ -31,10 +31,10 @@ in_room(Item, Room) :- item(Item), room(Room), \+ picked_up(Item).
 can_access(Bot, Room) :-
     room(Room),
     (
-        \+ requires_key(Room, _) %either no access because no key
-        ; % or
+        \+ requires_key(Room, _)
+        ;
         requires_key(Room, Key),
-        Bot = hold(Key, _) %%access because key
+        Bot = hold(Key, _)
     ).
 
 % Item has been picked up
@@ -43,9 +43,9 @@ picked_up(Item) :- pick_up(Item, _, _).
 %moves
 %allow the robot to move
 move(Bot, CurRoom, NextRoom):-
-    room(CurRoom).
-    room(NextRoom).
-    connected(CurRoom, NextRoom).
+    room(CurRoom),
+    room(NextRoom),
+    connected(CurRoom, NextRoom),
     robot(Bot).
 
 %The robot picks up an item in the room
@@ -55,15 +55,30 @@ pick_up(Item, Room, Bot) :-
     \+ Bot = hold(_, _),  % The robot cannot hold more than 2 items
     assert(picked_up(Item)).    % Mark it as picked up.
 
-%Let the robot drop a specific item
-drop(Item, Bot) :-
+% Let the robot drop a specific item
+drop(Item, Bot, Room) :-
     robot(Bot),
     Bot = hold(Item, _),
     retract(picked_up(Item)),   % Mark the item not picked up
-    assert(in_room(Item, Room)). % Place it in the room
+    assert(in_room(Item, Room)).   % Place it in the room
 
-%Queries
-? - move(Bot, CurRoom, NextRoom).
-? - can_access(Bot, Room).
-% ? - pick_up
-% ? - drop
+% Check if the win condition is met
+wincon :-
+    in_room(box, r2).
+
+% Solve the problem within N steps
+solveR(State, N, Trace) :-
+    solveR(State, State, N, [], Trace).
+
+solveR(_, _, 0, _, _) :-
+    fail. % No solution found within N steps.
+
+solveR(_, _, _, Trace, Trace) :-
+    wincon.
+
+solveR(State, Bot, N, AccTrace, Trace) :-
+    N > 0,
+    move(Bot, CurRoom, NextRoom),
+    drop(Item, Bot, CurRoom),
+    NewN is N - 1,
+    solveR(State, Bot, NewN, [drop(Item, NextRoom) | AccTrace], Trace).
